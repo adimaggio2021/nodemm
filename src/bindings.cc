315989dd46e5ad9491981c160b7bf5b363451ab2
Anthony DiMaggio <42413529+adimaggio2021@users.noreply.github.com>
2020-06-07 09:31:02 -0400

    hope 2

--- src/bindings.cc
+++ src/bindings.cc
@@ -183,7 +183,7 @@ void SimulateFromXml(const Nan::FunctionCallbackInfo<Value>& args) {
         if (IsString(Get(props, i))) {
             Local<String> key = props->Get(Nan::GetCurrentContext(), i).FromMaybe(v8::Local<v8::Value>())->ToString(Nan::GetCurrentContext()).FromMaybe(v8::Local<v8::String>());
             if (IsString(Get(files, key))) {
-                Nan::Utf8String val(files->Get(Nan::GetCurrentContext(), key).FromMaybe(v8::Local<v8::Array>())->ToString(Nan::GetCurrentContext()).FromMaybe(v8::Local<v8::String>()));
+                Nan::Utf8String val(files->Get(Nan::GetCurrentContext(), key).FromMaybe(v8::Local<v8::Value>())->ToString(Nan::GetCurrentContext()).FromMaybe(v8::Local<v8::String>()));
                 (*(baton->files))[string(*Nan::Utf8String(key))] = string(*val);
             }
         }
